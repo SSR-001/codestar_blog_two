60940272274040edc4f7c0d720eb8fe58811946f
SSR-001 <ssrichardson1010@gmail.com>
2025-10-15 14:27:27 +0100

    i dont even know

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,9 @@ from django.views import generic
 from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
-    # queryset = Post.objects.filter(author=4) #reluctant to delete currently
-    # queryset = Post.objects.all() #reluctant to delete currently
+    #reluctant to delete currently
+    # queryset = Post.objects.filter(author=4) 
+    #reluctant to delete currently
+    # queryset = Post.objects.all()
     template_name = "post_list.html"
     queryset = Post.objects.filter(status=1)
