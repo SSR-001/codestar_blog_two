b87cbbec3e686b9ceb93594051644506044b5d79
SSR-001 <ssrichardson1010@gmail.com>
2025-10-16 16:02:53 +0100

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     
-    #reluctant to delete currently:
-    # queryset = Post.objects.filter(author=4) 
-    #reluctant to delete currently:
-    # queryset = Post.objects.all()
-    
     template_name = "blog/index.html"
     paginate_by = 6
     queryset = Post.objects.filter(status=1)
+    
+def post_detail(request, slug):
+    """
+    display an individual :model:`blog.post`.
+    
+    **context**
+    
+    ``post``
+        an instance of :model:`blog.post`.
+        
+        **template:**
+        
+        :template:`blog/post_detail.html`
+    """
+    
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
