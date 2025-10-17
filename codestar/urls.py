9796bebcef6fb978df3c457aa8fdedd752a6a211
SSR-001 <ssrichardson1010@gmail.com>
2025-10-17 14:54:12 +0100

    small changes, I dunno

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path("about/", include("about.urls"), name="about"),
     path('admin/', admin.site.urls),
-    path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
+    
 ]
  
