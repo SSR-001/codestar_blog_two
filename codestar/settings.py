7e33f93d10cfe5bde5302b778e66d554727d557c
SSR-001 <ssrichardson1010@gmail.com>
2025-10-20 14:45:22 +0100

    add code for users adding comments

--- codestar/settings.py
+++ codestar/settings.py
@@ -49,7 +49,9 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'django',
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'django', #THESE MIGHT BE IN THE WRONG ORDER, IF THAT MATTERS. cHECK THE CI SOURCE CODE.
     'blog',
     'django_summernote',
     'about',
@@ -150,6 +152,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
